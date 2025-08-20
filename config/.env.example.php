9a1abfbd325f3f9f248595a94a0c411be7b919e5
Miraç <156301172+MrcDprm@users.noreply.github.com>
2025-08-20 17:31:24 +0300

    Update .env.example.php

--- config/.env.example.php
+++ config/.env.example.php
@@ -1,9 +1,8 @@
 <?php
-// Bu dosya sadece örnektir. Kendi şifrenizi girip dosya adını .env.php yapın.
 
 return [
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'master_db',
     'DB_USER' => 'root',
-    'DB_PASS' => 'parola',
+    'DB_PASS' => '',
 ];
